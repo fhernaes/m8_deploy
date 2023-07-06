44b1a12176c0e3a77597638cd22324b3d7824ddf
Fernando Hernández <f_hernandez_e@hotmail.com>
2023-07-05 21:44:09 -0400

    se añade rutas y métodos update y destroy

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -35,8 +35,65 @@ export const addProductos = async (req, res) => {
         });
     } catch (error) {
         console.log(error);
-        fs.unlinkSync(req.pathImagen);
+        fs.unlinkSync(req.pathImagen); //elimina la imagen
         res.status(500).json({code: 500, message:"Error al crear el producto en la base de datos."})
     }
 
 };
+// update productos
+export const updateProductos = async (req, res) => {
+ let { nombre, descripcion, precio } = req.body;
+    let { id } = req.params;
+    try {
+        let updateProducto = {
+            nombre,
+            descripcion,
+            precio: Number(precio),
+            img: req.nombreImagen,
+            rutaImagen: `/public/uploads/${req.nombreImagen}`,
+        }
+        let productoModificado = await Producto.update(updateProducto, {
+            where: { id: id },
+        });
+        
+        // Consultar el producto actualizado
+        let productoActualizado = await Producto.findByPk(id, {
+            attributes: { exclude: ["createdAt", "updatedAt"] },
+        });
+        
+        res.status(200).json({
+            code: 200,
+            message: "Producto modificado con éxito.",
+            data: productoActualizado,
+        });
+        
+
+    } catch (error) {
+        console.log(error);
+        fs.unlinkSync(req.pathImagen); //elimina la imagen
+        res.status(500).json({code: 500, message:"Error al modificar el producto en la base de datos."})
+    }
+};
+// delete productos
+export const deleteProductos = async (req, res) => {
+    let { id } = req.params;
+    try {
+        let productoEliminado = await Producto.findByPk(id, {
+            attributes: { exclude: ["createdAt", "updatedAt"] },
+        });
+        
+        await Producto.destroy({
+            where: { id: id },
+        });
+        
+        res.status(200).json({
+            code: 200,
+            message: "Producto eliminado con éxito.",
+            data: productoEliminado,
+        });
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({code: 500, message:"Error al eliminar el producto en la base de datos."})
+    }
+}
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { findAllProductos, addProductos } from "../controllers/productos.controllers.js";
+import { findAllProductos, addProductos, updateProductos, deleteProductos } from "../controllers/productos.controllers.js";
 import upload from "../middlewares/upload.middleware.js"
 const router = Router();
 
@@ -8,6 +8,15 @@ router.get("/", findAllProductos);
 
 //ruta post productos
 router.post("/", upload, addProductos);
+
+// ruta put productos
+router.put("/:id", upload ,updateProductos);
+
+// ruta delete productos
+router.delete("/:id", deleteProductos);
+// ruta destroy productos
+router.delete("/:id/destroy", deleteProductos);
+
 
 
 export default router;
